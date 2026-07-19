9a84b8c2a99b3d7d7a4ed58a916d07a34893f6b3
bagus778 <126353656+bagus778@users.noreply.github.com>
2026-07-19 13:11:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import { execSync } from 'child_process';
 
+console.log("=== [1/2] Mendeteksi Project Vite: Memulai Proses Build ===");
+execSync('npx vite build', { stdio: 'inherit' });
+
+console.log("=== [2/2] Proses Build Sukses: Menjalankan Server Preview ===");
+execSync('npx vite preview --host 0.0.0.0 --port 8443', { stdio: 'inherit' });
